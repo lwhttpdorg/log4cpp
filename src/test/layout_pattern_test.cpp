a5dc19f98cf365798e2a1a94a4fae3de4a2bb840
sandro <sandrodickens@gmail.com>
2025-01-08 21:26:17 +0800

    Set CMake Debug/Release postfix NULL

--- src/test/layout_pattern_test.cpp
+++ src/test/layout_pattern_test.cpp
@@ -195,6 +195,7 @@ TEST(layout_pattern_tests, day_format_test) {
 	EXPECT_EQ(strncmp(actual, expected, cmp_len), 0);
 }
 
+
 TEST(layout_pattern_tests, time_hour_format_test) {
 	tm now_tm{};
 	unsigned short ms;
