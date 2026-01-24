52e7254556fe03a45712c3760e72845fb9167055
lwhttpdorg <sandrodickens@gmail.com>
2026-01-25 00:57:56 +0800

    build: suppress the '-Wunused-result' warning

--- src/lib/appender/file_appender.cpp
+++ src/lib/appender/file_appender.cpp
@@ -77,7 +77,10 @@ namespace log4cpp::appender {
 #ifdef _MSC_VER
         (void)_write(this->fd, msg, static_cast<unsigned int>(msg_len));
 #else
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-result"
         (void)write(this->fd, msg, msg_len);
+#pragma GCC diagnostic pop
 #endif
     }
 } // namespace log4cpp::appender
